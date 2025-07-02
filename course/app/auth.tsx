28085f1c3d1957284c2085699e1e3d2c7ecaa8be
dzemil karisik <karisikdzemil@gmail.com>
2025-07-02 18:45:02 +0200

    changes

--- course/app/auth.tsx
+++ course/app/auth.tsx
@@ -1,21 +1,42 @@
 import { KeyboardAvoidingView, Platform, View, StyleSheet } from "react-native";
 import { useState } from "react";
 
-import { Button, Text, TextInput } from "react-native-paper";
+import { Button, Text, TextInput, useTheme } from "react-native-paper";
 
 export default function AuthScreen() {
   const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>("");
 
-    const handleSwitchMode = () => {
-        setIsSignUp(prev => !prev);
+  const theme = useTheme();
+
+  const handleAuth = async () => {
+    if(!email || !password){
+      setError("Please fill in all fields.");
+      return;
     }
+
+    if(password.length < 6){
+      setError("Password must be at least 6 characters long.");
+      return;
+    }  
+
+    setError(null);
+}
+
+  const handleSwitchMode = () => {
+    setIsSignUp((prev) => !prev);
+  };
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={styles.container}
     >
       <View style={styles.content}>
-        <Text style={styles.title} variant="headlineMedium">{isSignUp ? "Create Account" : "Welcome Back"}</Text>
+        <Text style={styles.title} variant="headlineMedium">
+          {isSignUp ? "Create Account" : "Welcome Back"}
+        </Text>
 
         <TextInput
           label="Email"
@@ -24,6 +45,7 @@ export default function AuthScreen() {
           placeholder="example@gmail.com"
           mode="outlined"
           style={styles.input}
+          onChangeText={setEmail}
         />
         <TextInput
           label="Password"
@@ -31,9 +53,19 @@ export default function AuthScreen() {
           keyboardType="email-address"
           mode="outlined"
           style={styles.input}
+          onChangeText={setPassword}
         />
-        <Button mode="contained" style={styles.button}>{isSignUp ? "Sign Up" : "Sign In"}</Button>
-        <Button mode="text" onPress={handleSwitchMode} style={styles.switchModeButton}>
+
+        {error && <Text style={{ color: theme.colors.error }}>{error}</Text>}
+
+        <Button mode="contained" style={styles.button} onPress={handleAuth}>
+          {isSignUp ? "Sign Up" : "Sign In"}
+        </Button>
+        <Button
+          mode="text"
+          onPress={handleSwitchMode}
+          style={styles.switchModeButton}
+        >
           {isSignUp
             ? "Already have an account? Sign In"
             : "Don't have an account? Sign Up"}
@@ -44,26 +76,26 @@ export default function AuthScreen() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "f5f5f5",
-    },
-    content: {
-        flex: 1,
-        padding: 16,
-        justifyContent: "center",
-    },
-    title: {
-        textAlign: "center",
-        marginBottom: 24
-    },
-    input: {
-        marginBottom: 16,
-    },
-    button: {
-        marginTop: 8,
-    },
-    switchModeButton: {
-        marginTop: 16,
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "f5f5f5",
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  input: {
+    marginBottom: 16,
+  },
+  button: {
+    marginTop: 8,
+  },
+  switchModeButton: {
+    marginTop: 16,
+  },
+});
---
+++ course/lib/auth-context.tsx
@@ -0,0 +1,45 @@
+import { createContext } from "react";
+import { Models } from "react-native-appwrite";
+
+type AuthContextType = {
+  user: Models.User<Models.Preferences> | null;
+  signUp: (email: string, password: string) => Promise<string | null>;
+  signIn: (email: string, password: string) => Promise<sgring | null>;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+
+    const signUp = async (email: string, password: string) => {
+        try{
+            await account.create(ID.unique(), email, password);
+            await signIn(email, password);
+            return null;
+        }catch(error){
+            if(error instanceof Error){
+                return error.message;
+            }
+            return "An error occured during signup"
+        }
+    }
+     const signIn = async (email: string, password: string) => {
+        try{
+            await account.create(ID.unique(), email, password);
+            return null;
+        }catch(error){
+            if(error instanceof Error){
+                return error.message;
+            }
+            return "An error occured during signup"
+        }
+    }
+  return (
+    <AuthContext.Provider value={{ user, signUp, sign }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {}
+ 
