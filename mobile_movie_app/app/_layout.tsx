94f8cd8126ce01ea3938506f958bb6bba528e9da
dzemil karisik <karisikdzemil@gmail.com>
2025-07-07 18:08:54 +0200

    assets and icons

--- mobile_movie_app/app/_layout.tsx
+++ mobile_movie_app/app/_layout.tsx
@@ -1,7 +1,12 @@
-import './globals.css';
+import "./globals.css";
 
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
