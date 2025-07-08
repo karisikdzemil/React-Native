a803540065a7648858cd0b8c35f5ae1745af0013
dzemil karisik <karisikdzemil@gmail.com>
2025-07-08 13:10:08 +0200

    changes

--- mobile_movie_app/app/(tabs)/_layout.tsx
+++ mobile_movie_app/app/(tabs)/_layout.tsx
@@ -23,7 +23,7 @@ const TabIcon = ( {focused, icon, title}: any ) => {
 
 return(
     <View className="size-full justify-center items-center mt-4 rounded-full">
-        <Image source={icon} tintColor="A8B5DB" className="size-5"/>
+        <Image source={icon} tintColor="#A8B5DB" className="size-5"/>
     </View>
 )
 
